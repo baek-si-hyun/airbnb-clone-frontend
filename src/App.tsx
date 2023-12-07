9fb9955f48ac318bd5e4f02c1575460202fe38c8
LAPTOP-SSV2GS54\qortl <qortlgus100@gmail.com>
2023-12-07 23:15:33 +0900

    FRONT-END SETUP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import { Text } from "@chakra-ui/react";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Text color={"red.500"} fontSize={"6xl"}>
+        It works!!!!!
+      </Text>
+    </div>
+  );
 }
 
 export default App;
