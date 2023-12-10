c6b7defdbfaff2dfd028a0f3b5a65cab3898aea6
LAPTOP-SSV2GS54\qortl <qortlgus100@gmail.com>
2023-12-10 17:27:19 +0900

    CHAKRA UI

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import { ChakraProvider } from "@chakra-ui/react";
-
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
